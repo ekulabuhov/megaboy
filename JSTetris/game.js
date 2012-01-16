64da032c346a50d94278738e1d5c28ef77a0bf17
hategrail@gmail.com <hategrail@gmail.com@7ceb8a7c-b436-0410-9139-7549c33871fe>
2012-01-16 18:28:27 +0000

    Added some more logic.

---
+++ JSTetris/game.js
@@ -0,0 +1,30 @@
+﻿goog.provide('game');
+goog.require('game.pieces');
+goog.require('game.board');
+
+game.WAIT_TIME = 700; 	 // Number of milliseconds that the piece remains before going 1 block down */
+
+/*
+ * Get a random int between to integers
+ */
+game.getRand = function (a, b) {
+	return Math.random() * b + a;
+	//return Math.random() % (b - a + 1) + a;
+}
+
+game.initGame = function () {
+// Init random numbers
+//srand ((unsigned int) time(NULL));
+
+// First piece
+game.piece	 = this.getRand (0, 6);
+mRotation	 = GetRand (0, 3);
+mPosX = (BOARD_WIDTH / 2) + mPieces.GetXInitialPosition (mPiece, mRotation);
+mPosY = mPieces.GetYInitialPosition (mPiece, mRotation);
+
+// Next piece
+mNextPiece = GetRand (0, 6);
+mNextRotation = GetRand (0, 3);
+mNextPosX = BOARD_WIDTH + 5;
+mNextPosY = 5;
+}
